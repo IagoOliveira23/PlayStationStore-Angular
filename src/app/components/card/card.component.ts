1cb823673eae0d5c0c1718b83618444786d44545
IagoOliveira23 <iago.ishift@gmail.com>
2023-09-21 10:09:04 -0300

    finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  
+  @Input()
+  gameCover:string=''
+  constructor(){}
 
+  @Input()
+  gameLabel:string = ""
+
+  @Input()
+  gameType:string='XPTO | PS4'
+
+  @Input()
+  gamePrice:string= 'R$ 219.99'
+
+  ngOnInit(): void {
+      
+  }
 }
